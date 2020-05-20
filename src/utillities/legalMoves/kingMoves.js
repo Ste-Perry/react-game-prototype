5cbfd3a808bce9d55f7cb15eff3d9b83646bbb1d
Jeffrey <openshiftninja@gmail.com>
2020-05-20 00:56:13 -0400

    king moves defined - ready to start game logic

--- src/utillities/legalMoves/kingMoves.js
+++ src/utillities/legalMoves/kingMoves.js
@@ -1,3 +1,27 @@
-const getKingMoves = () => [];
+import { getIdx } from '../rowColToIndexUtility';
+import { isEnemyPiece, maxBound, minBound } from '../movesUtility';
+import { getSquareNameFromRowCol } from '../squareNameUtility';
+import pieceTypes from '../../constants/pieceTypes';
+
+const getKingMoves = (isWhite, row, col, squares) => {
+  const moves = [];
+  const checkMove = (checkRow, checkCol, checkRowBound, checkColBound) => {
+    if (checkRowBound(checkRow) && checkColBound(checkCol)) {
+      const pieceType = squares[getIdx(checkRow, checkCol)].piece.type;
+      if (pieceType === pieceTypes.EMPTY_SQUARE || isEnemyPiece(pieceType, isWhite)) {
+        moves.push(getSquareNameFromRowCol(checkRow, checkCol));
+      }
+    }
+  };
+  checkMove(row - 1, col + 1, minBound, maxBound); // north east
+  checkMove(row, col + 1, () => true, maxBound); // east
+  checkMove(row + 1, col + 1, maxBound, maxBound); // south east
+  checkMove(row + 1, col, maxBound, () => true); // south
+  checkMove(row + 1, col - 1, maxBound, minBound); // south west
+  checkMove(row, col - 1, () => true, minBound); // west
+  checkMove(row - 1, col - 1, minBound, minBound); // north west
+
+  return moves;
+};
 
 export default getKingMoves;
--- src/utillities/legalMoves/legalMoves.js
+++ src/utillities/legalMoves/legalMoves.js
@@ -5,14 +5,74 @@ import getKnightMoves from './knightMoves';
 import getBishopMoves from './bishopMoves';
 import getQueenMoves from './queenMoves';
 import getKingMoves from './kingMoves';
-import { getIdx } from '../rowColToIndexUtility';
+import { getIdx, getRowCol } from '../rowColToIndexUtility';
+import { getRowColFromSquareName } from '../squareNameUtility';
+import { isEnemyPiece } from '../movesUtility';
 
 const getLegalMoves = (row, col, squares) => {
-  const getPieceType = () => {
-    return squares[getIdx(row, col)].piece.type;
+  const canPieceAttack = (isWhite, pieceType, targetRow, targetCol, pieceRow, pieceCol, getMoves) => {
+    if (isEnemyPiece(pieceType, isWhite)) {
+      const possibleMoves = getMoves(!isWhite, pieceRow, pieceCol, squares);
+      for (let jdx = 0; jdx < possibleMoves.length; jdx += 1) {
+        const { row: moveRow, col: moveCol } = getRowColFromSquareName(possibleMoves[jdx]);
+        if (targetRow === moveRow && targetCol === moveCol) {
+          return true;
+        }
+      }
+    }
+    return false;
   };
 
-  const pieceType = getPieceType();
+  const getMovesMethodFromType = pieceType => {
+    switch (pieceType) {
+      case pieceTypes.WHITE_PAWN:
+      case pieceTypes.BLACK_PAWN:
+        return getPawnMoves;
+      case pieceTypes.WHITE_ROOK:
+      case pieceTypes.BLACK_ROOK:
+        return getRookMoves;
+      case pieceTypes.WHITE_KNIGHT:
+      case pieceTypes.BLACK_KNIGHT:
+        return getKnightMoves;
+      case pieceTypes.WHITE_BISHOP:
+      case pieceTypes.BLACK_BISHOP:
+        return getBishopMoves;
+      case pieceTypes.WHITE_QUEEN:
+      case pieceTypes.BLACK_QUEEN:
+        return getQueenMoves;
+      case pieceTypes.WHITE_KING:
+      case pieceTypes.BLACK_KING:
+        return getKingMoves;
+      case pieceTypes.EMPTY_SQUARE:
+      default:
+        return () => [];
+    }
+  };
+
+  const canEnemyAttack = (isWhite, targetRow, targetCol) => {
+    for (let idx = 0; idx < 64; idx += 1) {
+      const pieceType = squares[idx].piece.type;
+      const { row: pieceRow, col: pieceCol } = getRowCol(idx);
+      if (canPieceAttack(isWhite, pieceType, targetRow, targetCol, pieceRow, pieceCol, getMovesMethodFromType(pieceType))) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const getLegalKingMoves = isWhite => {
+    const kingMoves = [];
+    const possibleMoves = getKingMoves(isWhite, row, col, squares);
+    possibleMoves.forEach(move => {
+      const { row: squareRow, col: squareCol } = getRowColFromSquareName(move);
+      if (!canEnemyAttack(isWhite, squareRow, squareCol)) {
+        kingMoves.push(move);
+      }
+    });
+    return kingMoves;
+  };
+
+  const pieceType = squares[getIdx(row, col)].piece.type;
   let moves;
   switch (pieceType) {
     case pieceTypes.BLACK_PAWN:
@@ -37,7 +97,7 @@ const getLegalMoves = (row, col, squares) => {
       break;
     case pieceTypes.BLACK_KING:
     case pieceTypes.WHITE_KING:
-      moves = getKingMoves(pieceType === pieceTypes.WHITE_KING, row, col, squares);
+      moves = getLegalKingMoves(pieceType === pieceTypes.WHITE_KING);
       break;
     case pieceTypes.EMPTY_SQUARE:
     default:
